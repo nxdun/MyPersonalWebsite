6f7cceb929d651c0e7e87d27f3c96b0b6c7fae56
Nadun L <92899660+nxdun@users.noreply.github.com>
2022-12-24 13:29:23 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 function lightsOn(){
-    document.getElementyId("id-js").
+    var x = document.body.style.backgroundImage;
     
+    var y = 'url("../images/room-wall.jpg")';
+    console.log(y+"="+x);
+
+    if(x == y){
+        document.body.style.backgroundImage = "url(../images/room-win.jpg)";
+        
+
+    }
+
+    else{
+    document.body.style.backgroundImage = "url(../images/room-wall.jpg)";
+    }
+
 }
